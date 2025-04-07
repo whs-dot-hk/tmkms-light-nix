025335d9675a58089b1b2a1b575cfea4dd5b749b
whs <hswongac@gmail.com>
2025-04-07 11:39:39 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+  inputs.aws-nitro-enclaves-sdk-c-nix.url = "github:whs-dot-hk/aws-nitro-enclaves-sdk-c-nix";
   inputs.tmkms-light.url = "github:crypto-com/tmkms-light";
 
   inputs.tmkms-light.flake = false;
@@ -15,10 +16,32 @@
     in {
       packages.default = pkgs.rustPlatform.buildRustPackage {
         cargoBuildFlags = "-p tmkms-nitro-helper";
-        cargoHash = "sha256-HmjR6/2oQsv8vP/eSvHf4L4x3Nwl69zTY6d3P2eh3YM=";
+        cargoHash = "sha256-bgjqNZvcIsCQHEII6QrHQ76SseL7Iay+21oZaaHoBws=";
         pname = "tmkms-light";
         src = inputs.tmkms-light;
         version = "1.0.0";
       };
+      packages.docker = let
+        app = pkgs.rustPlatform.buildRustPackage {
+          cargoBuildFlags = "-p tmkms-nitro-enclave";
+          cargoHash = "sha256-bgjqNZvcIsCQHEII6QrHQ76SseL7Iay+21oZaaHoBws=";
+          pname = "tmkms-nitro-enclave";
+          src = inputs.tmkms-light;
+          version = "1.0.0";
+          buildInputs = [inputs.aws-nitro-enclaves-sdk-c-nix.packages.${system}.aws-nitro-enclaves-sdk-c];
+          nativeBuildInputs = [pkgs.pkg-config];
+        };
+      in
+        pkgs.dockerTools.buildImage {
+          name = "tmkms-nitro-enclave";
+          tag = "nix";
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = [pkgs.dockerTools.caCertificates];
+          };
+          config = {
+            Entrypoint = ["${app}/bin/tmkms-nitro-enclave"];
+          };
+        };
     });
 }
